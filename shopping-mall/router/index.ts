af4b2611ab0e464b1d279ad224f02a60ce3cd8d4
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-03-21 00:47:53 +0900

    feat(shop): Auth API 기능 구현

--- shopping-mall/router/index.ts
+++ shopping-mall/router/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { authRouter } from "./authRouter";
 import { productRouter } from "./productRouter";
 
 const router = express.Router();
 
 router.use("/product", productRouter);
+router.use("/auth", authRouter);
 
 export { router };
